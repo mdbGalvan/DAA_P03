7394b155517018887781d2ad8b86f3b12b50178d
mdbGalvan <magomenlopark@gmail.com>
2014-02-24 02:24:10 +0000

    Attribute
    He añadido los atributos pensando en la clase monomio.

--- Polinomio.cpp
+++ Polinomio.cpp
@@ -1,21 +1,40 @@
 #include "Polinomio.h"
 
-
+void Polinomio::reservar(int n){
+	m_ = new (nothrow)Monomio[n];
+	if (!m_) {
+		cerr << "Error: no hay memoria";
+		exit(1);
+	}
+}void Polinomio::liberar(){
+	delete[] m_;
+}
+// CONSTRUCTORES
 Polinomio::Polinomio()
 {
-	gr_ = 10;
-	ter_ = 0;
-	m_ = new Monomio[gr_ + 2];
+	ter_ = 10;
+	m_ = new Monomio[ter_ + 2];
+	
 }
 
-Polinomio::Polinomio(int g)
+Polinomio::Polinomio(int ter)
 {
-	gr_ = g;
 	ter_ = 0;
+	m_ = new Monomio[ter_ + 2];
+}
+
+Polinomio::Polinomio(const Polinomio &p)
+{
+	gr_ = p.gr_;
 	m_ = new Monomio[gr_ + 2];
+
+	for (register int i = 0; i < gr_ + 1; i++) m_[i] = p.m_[i];
 }
 
 
+
+// DESTRUCTOR
 Polinomio::~Polinomio()
 {
+	liberar();
 }
--- Polinomio.h
+++ Polinomio.h
@@ -9,10 +9,14 @@ class Polinomio {
 		int gr_;								// Grado del Polinomio
 		int ter_;								// Número de términos en el polinomio
 		Monomio *m_;							// Array de monomios
+
+		void reservar(int n);					// Reservar memoria para m_
+		void liberar();							// Liberar memoria para m_
 	public:
 		Polinomio();							// Constructor por defecto, gr = 10
 		Polinomio(int g);						// Constructor fijando el grado
 		Polinomio(int coef[], const int tam);	// Constructor con vector de coeficientes
+		Polinomio(const Polinomio &p);			// Constructor copia
 		~Polinomio();
 };
 
