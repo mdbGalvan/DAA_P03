ec54687d7c0c146093d9f01088e3130146bba483
mdbGalvan <magomenlopark@gmail.com>
2014-02-24 02:43:43 +0000

    Atributo

--- Polinomio.cpp
+++ Polinomio.cpp
@@ -1,37 +1,62 @@
 #include "Polinomio.h"
 
+// Reservar memoria para coef_
 void Polinomio::reservar(int n){
-	m_ = new (nothrow)Monomio[n];
-	if (!m_) {
+	coef_ = new (nothrow)int[n];
+	if (!coef_) {
 		cerr << "Error: no hay memoria";
 		exit(1);
 	}
-}void Polinomio::liberar(){
-	delete[] m_;
+}// Liberar memoria para coef_void Polinomio::liberar(){
+	delete[] coef_;
 }
 // CONSTRUCTORES
 Polinomio::Polinomio()
 {
+	gr_ = 0;
 	ter_ = 10;
-	m_ = new Monomio[ter_ + 2];
+	coef_ = new int[ter_ + 1];
 	
 }
 
 Polinomio::Polinomio(int ter)
 {
-	ter_ = 0;
-	m_ = new Monomio[ter_ + 2];
+	gr_ = 0;
+	ter_ = ter;
+	coef_ = new int[ter_ + 1];
 }
 
 Polinomio::Polinomio(const Polinomio &p)
 {
 	gr_ = p.gr_;
-	m_ = new Monomio[gr_ + 2];
+	ter_ = p.ter_;
+	coef_ = new int[ter_ + 2];
 
-	for (register int i = 0; i < gr_ + 1; i++) m_[i] = p.m_[i];
+	for (register int i = 0; i < gr_ + 1; i++) coef_[i] = p.coef_[i];
 }
 
+// Establecer el valor del coef_ de grado i
+void Polinomio::setCoef(int i, int valor)
+{
+	if (i<0 && i>ter_) {			// Si es mayor que el grado máximo o menor que 0 devuelve error
+		cerr << "Error: grado incorrecto";
+		exit(1);
+	}
+	coef_[i] = valor;
+	if (i > gr_)					// Si el grado es mayor que el del polinomio se restablece
+		gr_ = i;
+}
 
+int Polinomio::Evaluar(int x) const
+{
+	int aux = 1;							// Almacenará x^exp
+
+	if (exp_ > 0)
+	for (int i = 0; i < getExp(); i++)
+		aux *= x;
+
+	return getCoef() * aux;
+}
 
 // DESTRUCTOR
 Polinomio::~Polinomio()
--- Polinomio.h
+++ Polinomio.h
@@ -7,16 +7,20 @@
 class Polinomio {
 	private:
 		int gr_;								// Grado del Polinomio
-		int ter_;								// Número de términos en el polinomio
-		Monomio *m_;							// Array de monomios
+		int ter_;								// Número de términos máximo en el polinomio
+		int *coef_;								// Array de coeficientes
 
-		void reservar(int n);					// Reservar memoria para m_
-		void liberar();							// Liberar memoria para m_
+		void reservar(int n);					// Reservar memoria para coef_
+		void liberar();							// Liberar memoria para coef_
 	public:
 		Polinomio();							// Constructor por defecto, gr = 10
-		Polinomio(int g);						// Constructor fijando el grado
+		Polinomio(int ter = 10);				// Constructor fijando el grado
 		Polinomio(int coef[], const int tam);	// Constructor con vector de coeficientes
 		Polinomio(const Polinomio &p);			// Constructor copia
+
+		void setCoef(int i, int valor);			// Establecer el valor del coef_ de grado i
+		int Evaluar(int x) const;				// Evalua un monomio en un punto
+
 		~Polinomio();
 };
 
