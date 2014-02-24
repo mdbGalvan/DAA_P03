cc3098a84442f051f6a58a07e337aa5201b34f00
mdbGalvan <magomenlopark@gmail.com>
2014-02-24 03:13:07 +0000

    Add Evaluar Polinomio

--- Polinomio.cpp
+++ Polinomio.cpp
@@ -1,65 +1,58 @@
 #include "Polinomio.h"
 
-// Reservar memoria para coef_
-void Polinomio::reservar(int n){
-	coef_ = new (nothrow)int[n];
-	if (!coef_) {
-		cerr << "Error: no hay memoria";
-		exit(1);
-	}
-}// Liberar memoria para coef_void Polinomio::liberar(){
-	delete[] coef_;
-}
 // CONSTRUCTORES
 Polinomio::Polinomio()
 {
 	gr_ = 0;
-	ter_ = 10;
-	coef_ = new int[ter_ + 1];
-	
+	ter_ = 0;	
 }
 
-Polinomio::Polinomio(int ter)
+Polinomio::Polinomio(const int ter)
 {
 	gr_ = 0;
 	ter_ = ter;
-	coef_ = new int[ter_ + 1];
+	m_[ter_];
 }
 
+// Constructor Copia
 Polinomio::Polinomio(const Polinomio &p)
 {
 	gr_ = p.gr_;
 	ter_ = p.ter_;
-	coef_ = new int[ter_ + 2];
+	m_[ter_];
 
-	for (register int i = 0; i < gr_ + 1; i++) coef_[i] = p.coef_[i];
+	for (int i = 0; i < ter_; i++) m_[i] = p.m_[i];
 }
 
-// Establecer el valor del coef_ de grado i
-void Polinomio::setCoef(int i, int valor)
+// Constructor con vector de coeficientes
+Polinomio::Polinomio(int coef[], const int tam)
 {
-	if (i<0 && i>ter_) {			// Si es mayor que el grado máximo o menor que 0 devuelve error
-		cerr << "Error: grado incorrecto";
-		exit(1);
+	Monomio m; 
+
+	gr_ = tam;
+	ter_ = tam;
+	m_[ter_];
+
+	for (int i = 0; i < tam; i++) {
+		m.setCoef(coef[i]);
+		m.setExp(i);
+		m_[i] = m;
 	}
-	coef_[i] = valor;
-	if (i > gr_)					// Si el grado es mayor que el del polinomio se restablece
-		gr_ = i;
 }
 
 int Polinomio::Evaluar(int x) const
 {
-	int aux = 1;							// Almacenará x^exp
+	int aux = 0;							// Almacenará x^exp
 
-	if (exp_ > 0)
-	for (int i = 0; i < getExp(); i++)
-		aux *= x;
+	for (int i = 0; i < ter_; i++)
+		aux += m_[i].Evaluar(x);
 
-	return getCoef() * aux;
+	return aux;
 }
 
 // DESTRUCTOR
 Polinomio::~Polinomio()
 {
-	liberar();
+	ter_ = 0;
+	gr_ = 0;
 }
