f3afd2d656ab328f5b25b222da6a46bde2cc9d96
mdbGalvan <magomenlopark@gmail.com>
2014-03-06 03:13:29 +0000

    Test Polinomio

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include "Header.h"
-#include "Polinomio.h"	// Clase que maneja los polinomio
-
+#include "Tests.h"		// Clase que maneja los test
 
 int main(int argc, char* argv[]) {
 
 
-	Polinomio p, q;
-	cin >> p >> q;
-	cout << (p * q);
+	Polinomio p(2, 0, 10), q(3, 10, 100);
+	//cin >> p >> q;
+	cout << "p: " << p << endl;
+	cout << "q: " << q << endl << endl;
+	cout << "p + q: " << (p + q) << endl << endl;
+	cout << "p * q: " << (p * q) << endl << endl;
+	cout << "p(2): " << (p.Evaluar(2)) << endl << endl;
+	cout << "q(2): " << (q.Evaluar(2)) << endl << endl;
 
 
 	cin.ignore();
